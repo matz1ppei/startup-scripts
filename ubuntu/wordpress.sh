cf2a0efab306a838349b5d26f33150cac041921b
Ippei Matsubara <matz1ppei@gmail.com>
2025-09-01 10:20:07 +0900

    fix(nginx): Nginx設定の構文エラーとwwwサブドメインの対応を修正

--- ubuntu/wordpress.sh
+++ ubuntu/wordpress.sh
@@ -112,20 +112,20 @@ PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')
 sudo bash -c "cat > /etc/nginx/sites-available/${DOMAIN_NAME}" <<EOF
 server {
     listen 80;
-    server_name ${DOMAIN_NAME};
+    server_name ${DOMAIN_NAME} www.${DOMAIN_NAME};
     root ${WEB_ROOT};
 
     index index.php index.html index.htm;
 
     location / {
-        try_files $uri $uri/ /index.php?$args;
+        try_files \$uri \$uri/ /index.php?i\$args;
     }
 
     location ~ \.php\$
  {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php${PHP_VERSION}-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         include fastcgi_params;
     }
 
